6b214103b64ea84007df122552b16d4de793ebec
Anirudh <anirudh3578@gmail.com>
2026-07-07 06:30:36 +0530

    added react for frontend so navigation of pages can be handled and made the backend server data showing from all in single page to one actress per page using object id as key

---
+++ Frontend/react/src/pages/FavActressPage.jsx
@@ -0,0 +1,7 @@
+function FaveActressPage() {
+    return (
+        <h1>Actress view page</h1>
+    )
+}
+
+export default FavActressPage;
---
+++ Frontend/simple/app.js
@@ -0,0 +1,1 @@
+const startClick = document.querySelector('.class');
