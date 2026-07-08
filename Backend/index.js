704ae97eed6e124707eb9d6f755617fd57d88c51
Anirudh <anirudh3578@gmail.com>
2026-07-08 07:10:55 +0530

    added get request with useEffect hook in frontend to send request with id and in backend getting the request and sending the fake json data to be fetched by frontend and shown

--- Backend/index.js
+++ Backend/index.js
@@ -25,10 +25,35 @@ const favActresses = {
     }
 };
 
+// fake db for json data sending (duplicate of above favActresses - object of ojects)
+const actresses = {
+    "1": {
+        "name": "Leila George",
+        "imgSrc": "http://localhost:3000/images/leila-george.jpg",
+        "firstSaw": "Disclaimer"
+    },
+    "2": {
+        "name": "Jessica Brown Findlay",
+        "imgSrc": "http://localhost:3000/images/jessica-brown-findlay.jpg",
+        "firstSaw": "Brave New World"
+    },
+    "3": {
+        "name": "Sophia Myles",
+        "imgSrc": "http://localhost:3000/images/sophia-myles.jpg",
+        "firstSaw": "Hallam Foe"
+    }
+}
+
 app.get('/favActress/:id', (req, res) => {
     const { id } = req.params;
     const particularActress = favActresses[id];
     res.render('actress-page', { particularActress });
+})
+
+app.get('/actress/:id', (req, res) => {
+    console.log('got get request from react frontend');
+    const {id} = req.params;
+    res.send(actresses[id]);
 })
 
 app.listen(3000, () => {
